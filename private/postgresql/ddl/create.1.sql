aed0bcf86fffbdd827606fcbb5e6e91f668b194a
alex <alex@nabu.be>
2016-10-21 14:49:33 +0200

    Updated

--- private/postgresql/ddl/create.1.sql
+++ private/postgresql/ddl/create.1.sql
@@ -1,1 +1,2 @@
 alter table node_relations add constraint relation_unique unique(source_id, target_id, relation_type_id);
+alter table actions add constraint action_unique unique(name, component_id);
