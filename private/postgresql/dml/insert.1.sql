7824a43edbb19288b73a01065bb4234eed021fcc
alex <alex@nabu.be>
2016-10-21 17:11:43 +0200

    Updated

--- private/postgresql/dml/insert.1.sql
+++ private/postgresql/dml/insert.1.sql
@@ -20,3 +20,25 @@ insert into components (
 	timestamp '2016-10-07 14:35:42.435',
 	'user'
 );
+insert into audit_levels (
+	id,
+	created,
+	modified,
+	name
+) values (
+	'b8d812e1-17f8-4a66-b577-04681fb198ab',
+	timestamp '2016-10-21 15:07:54.862',
+	timestamp '2016-10-21 15:07:54.862',
+	'info'
+);
+insert into audit_levels (
+	id,
+	created,
+	modified,
+	name
+) values (
+	'21ee67b7-5e54-4ff5-b1eb-fc5c8c348e70',
+	timestamp '2016-10-21 15:08:13.138',
+	timestamp '2016-10-21 15:08:13.138',
+	'error'
+);
