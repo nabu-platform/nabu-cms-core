a958dc93b4f82bd41e308c3b999418aecbef90a9
Alexander Verbruggen <alex@nabu.be>
2018-06-25 11:06:30 +0200

    updated

--- manage/node/component/public/artifacts/component.js
+++ manage/node/component/public/artifacts/component.js
@@ -1,5 +1,18 @@
 window.addEventListener("load", function() {
 	application.bootstrap(function($services) {
+		
+		var components = ${json.stringify(structure(components: nabu.cms.core.database.component.selectAll()/results))};
+		
+		nabu.page.provide("page-enumerate", {
+			name: "component",
+			namespace: "nabu.cms",
+			enumerate: function() {
+				return components;
+			},
+			label: "name",
+			value: "id"
+		});
+		
 		nabu.page.provide("page-format", {
 			format: function(id) {
 				return $services.node.resolve(id).name;
